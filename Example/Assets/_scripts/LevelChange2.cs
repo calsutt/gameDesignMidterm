1d5b48b144380522be3744203ed35b99f072fa64
calsutt <calsut8@gmail.com>
2017-10-19 18:23:27 -0400

    Calvin fixes his levels a bit
    Yeah. level fixing

---
+++ Example/Assets/_scripts/LevelChange2.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class LevelChange2 : MonoBehaviour {
+
+	// Use this for initialization
+
+	void OnTriggerEnter2d(Collision2D other){
+		SceneManager.LoadScene ("scene3");
+		Debug.Log ("Here");
+	}
+
+}
